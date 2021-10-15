a6f62c6815be8c8f9c8f597be68b43e6e1ab1277
JulyKnot <julyknot2049@gmail.com>
2021-10-15 16:30:55 -0300

    Carrito en proceso

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,7 @@ function mostrarCarro(){
     let itemsCarro = localStorage.getItem('Producto en carrito');
     itemsCarro= JSON.parse(itemsCarro);
     let contenedorProductos = document.querySelector('.products');
+    let costoCarrito = localStorage.getItem('Costo total ');
     if(itemsCarro && contenedorProductos){
         contenedorProductos.innerHTML = '';
         Object.values(itemsCarro).map(item => {
@@ -11,10 +12,31 @@ function mostrarCarro(){
             <div class='product'>
             <ion-icon name="close-circle-outline"></ion-icon> 
             <img src="../assets/images/${item.nombre}.jpg">
-            <span>${item.tag}<span>
+            <span>${item.tag}   <span>
             </div>
-            <div class ="precio">${item.precio}</div>`
+            <div class ="precio"
+            >$${item.precio}  
+            </div>
+            <div class ="cantidad">
+            <ion-icon name="arrow-back-circle-outline"></ion-icon>
+            <span>${item.enCarrito}  <span>
+            <ion-icon name="arrow-forward-circle-outline"></ion-icon>
+            </div>
+            <div class="total">
+            $${item.enCarrito * item.precio},00
+            </div>
+            `;
         });
+
+        contenedorProductos.innerHTML += `
+        <div class="contenedorTotal">
+        <h4 class="conTitulo">
+            Total
+        </h4>
+        <h4 class="cartTotal">
+        $${costoCarrito},00
+        </h4>
+        `;
     }  
 }
 
