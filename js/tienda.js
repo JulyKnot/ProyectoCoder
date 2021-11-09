d789f5073f364b4d71dbccf156a61c5c5d0dff94
JulyKnot <julyknot2049@gmail.com>
2021-11-08 23:03:02 -0300

    Agregar al Carrito Final

--- js/tienda.js
+++ js/tienda.js
@@ -3,46 +3,45 @@ const addToShoppingCartButtons = document.querySelectorAll(".buy-button");
 
 addToShoppingCartButtons.forEach(addToCartButton => {
     addToCartButton.addEventListener('click', AddToCartClicked);
-    });
+});
 
 
-    const comprarButton = document.querySelector('.comprarButton'
-    );
-    comprarButton.addEventListener('click' , comprarButtonClicked);
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
 
 
 const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer');
 
-    function AddToCartClicked(event) {
-        const button = event.target;
-        const item = button.closest('.card');
-        const itemTitle = item.querySelector('.title').textContent;
-        const itemPrice = item.querySelector('.text').textContent;
-        const itemImage = item.querySelector('.image img').src;
-        
+function AddToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest('.card');
+    const itemTitle = item.querySelector('.title').textContent;
+    const itemPrice = item.querySelector('.text').textContent;
+    const itemImage = item.querySelector('.image img').src;
 
-        addItemToShoppingCart(itemTitle,itemPrice,itemImage);
 
-    }
+    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 
-function addItemToShoppingCart(itemTitle,itemPrice,itemImage){
-   
+}
+
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+
     const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
         'shoppingCartItemTitle'
-      );
-      for (let i = 0; i < elementsTitle.length; i++) {
+    );
+    for (let i = 0; i < elementsTitle.length; i++) {
         if (elementsTitle[i].innerText === itemTitle) {
-          let elementQuantity = elementsTitle[
-            i
-          ].parentElement.parentElement.parentElement.querySelector(
-            '.shoppingCartItemQuantity'
-          );
-          elementQuantity.value++;
-          updateShoppingCartTotal();
-          return;
+            let elementQuantity = elementsTitle[
+                i
+            ].parentElement.parentElement.parentElement.querySelector(
+                '.shoppingCartItemQuantity'
+            );
+            elementQuantity.value++;
+            updateShoppingCartTotal();
+            return;
         }
-      }
-    const shoppingCartRow =  document.createElement('div');
+    }
+    const shoppingCartRow = document.createElement('div');
     const shoppingCartContent = `
     <div class="row shoppingCartItem">
         <div class="col-6">
@@ -66,50 +65,47 @@ function addItemToShoppingCart(itemTitle,itemPrice,itemImage){
         </div>
     </div>`
 
-shoppingCartRow.innerHTML = shoppingCartContent
-shoppingCartItemsContainer.append(shoppingCartRow)
+    shoppingCartRow.innerHTML = shoppingCartContent
+    shoppingCartItemsContainer.append(shoppingCartRow)
 
-shoppingCartRow.querySelector('.buttonDelete').addEventListener('click' , removeShoppingCartItem);
+    shoppingCartRow.querySelector('.buttonDelete').addEventListener('click', removeShoppingCartItem);
 
-shoppingCartRow.querySelector('.shoppingCartItemQuantity').addEventListener('change' , quantityChanger);
+    shoppingCartRow.querySelector('.shoppingCartItemQuantity').addEventListener('change', quantityChanger);
 
-updateShoppingCartTotal();
+    updateShoppingCartTotal();
 }
 
-function updateShoppingCartTotal(){
-    let total = 0 ;
+function updateShoppingCartTotal() {
+    let total = 0;
     const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
-    
+
     const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
 
     shoppingCartItems.forEach(shoppingCartItem => {
-    const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice'
-    );
-    const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('$','')
-    );
-    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity'
-    );
-    const shoppingCartItemQuantity = Number(
-        shoppingCartItemQuantityElement.value
-    );
-        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;  
+        const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+        const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('$', ''));
+        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity');
+        const shoppingCartItemQuantity = Number(
+            shoppingCartItemQuantityElement.value
+        );
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
     });
     shoppingCartTotal.innerHTML = `$${total},00`;
 }
 
-function removeShoppingCartItem(event){
+function removeShoppingCartItem(event) {
     const buttonClicked = event.target;
     buttonClicked.closest('.shoppingCartItem').remove();
     updateShoppingCartTotal();
 
 }
 
-function quantityChanger(event){
-   const input = event.target;
-   if (input.value <= 0){
-       input.value = 1;
-   }
-   updateShoppingCartTotal();
+function quantityChanger(event) {
+    const input = event.target;
+    if (input.value <= 0) {
+        input.value = 1;
+    }
+    updateShoppingCartTotal();
 }
 
 function comprarButtonClicked() {
